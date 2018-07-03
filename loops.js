00310333698348f6507096a13e68ee11f0b4b4a8
Scoti Dodson <scotidodson@gmail.com>
2018-07-03 02:13:04 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,7 @@
 
 function forLoop(array) {
   for (var i = 0; i<25; i++) {
-    if(i===1) {array.push(`I am ${i} strange loop.`);
+    if(i === 1) {array.push(`I am ${i} strange loop.`);
     }
   else {array.push(` am ${i} strange loops.`);
     }
