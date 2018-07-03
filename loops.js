d03b087a50e2285ceb9105cf33ea69bf2a04700b
Scoti Dodson <scotidodson@gmail.com>
2018-07-03 02:06:29 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,10 @@
 function forLoop(array) {
   var i
-  for (let i = 1; i<25; i++) {
+  for (let i = 1; i===25; i++) {
     console.log(`I am 1 strange loop.`);
     }
+  for (let i=2; i<25; i++) {
+    console.log('I am ${i} strange loops.')
+  }
 }
 
