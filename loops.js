c1f6d0572b46f28bae64a1953adbd79650168583
Scoti Dodson <scotidodson@gmail.com>
2018-07-03 02:03:37 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,10 @@
+function forLoop(array) {
+  var i
+  for (let i = 1; i===1; i++) {
+    console.log(`I am 1 strange loop.`);
+    }
+  for (let i = 1; 1 > i < 25; i++) {
+    console.log(`I am ${i} strange loops.`);
+  }
+}
+
